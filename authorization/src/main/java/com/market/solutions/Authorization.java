3011ad80b33402ee6bfa751b24981dbd072f8c13
dmtereshchenko <tereshchenkodm@yandex.ru>
2023-09-15 14:17:13 +0300

    Repository initialization

---
+++ authorization/src/main/java/com/market/solutions/Authorization.java
@@ -0,0 +1,11 @@
+package com.market.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Authorization {
+    public static void main(String[] args) {
+        SpringApplication.run(Authorization.class, args);
+    }
+}
